9d23ba0859524728cb69edea2f77de09d0303feb
Nassim <nassim.ouhenia@yahoo.com>
2019-05-21 19:51:08 +0200

    !

--- read.c
+++ read.c
@@ -5,7 +5,7 @@
 int main(void) {
 
   int i=O_RDWR;//|O_CREAT| O_EXCL;
-  MESSAGE* mess=msg_connect("a",i,4,8);
+  MESSAGE* mess=msg_connect("aw",i,4,8);
 
   if(mess == NULL) {
     perror("erreur de connexion");
@@ -16,7 +16,7 @@ int main(void) {
   printf("last = %d\n",mess->files->last);
 
   //int t=msg_send(mess, "moi", 3);
-  
+
   char * hh;
   printf("apre send\n");
   printf("first = %d\n",mess->files->first);
